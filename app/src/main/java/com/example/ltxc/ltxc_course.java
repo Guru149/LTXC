af2f9310c1d98f514d3824db59652e67a7a26135
Guru149 <ltxcproject01@gmail.com>
2021-01-04 10:21:32 +0530

    LTXC PROJECT FILE 2.0

--- app/src/main/java/com/example/ltxc/ltxc_course.java
+++ app/src/main/java/com/example/ltxc/ltxc_course.java
@@ -1,7 +1,9 @@
 package com.example.ltxc;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,10 +13,20 @@ import android.view.ViewGroup;
 public class ltxc_course extends Fragment {
 
 
+    @SuppressLint("NewApi")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ltxc_course, container, false);
+       View root =inflater.inflate(R.layout.fragment_ltxc_course, container, false);
+        CardView cardView = root.findViewById(R.id.about_us_card);
+        return root;
+       /* cardView.setOnContextClickListener(new View.OnContextClickListener() {
+            @Override
+            public boolean onContextClick(View v) {
+               // View inflate = inflater.inflate(getContext(), ltxc_login.class);
+                return false;
+            }
+        });*/
     }
 }
