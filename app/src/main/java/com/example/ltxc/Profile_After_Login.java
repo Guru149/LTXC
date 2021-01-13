ea4c04018ff28d41a41a75f8d0217b1144906d9d
Guru149 <ltxcproject01@gmail.com>
2021-01-13 10:15:44 +0530

    13 th jan 2021 Authentication updated

---
+++ app/src/main/java/com/example/ltxc/Profile_After_Login.java
@@ -0,0 +1,14 @@
+package com.example.ltxc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile_After_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile__after__login);
+    }
+}
